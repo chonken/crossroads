b91afe263027182e99886fc513f1515e91ceb5f6
chonken <liaochonken@gmail.com>
2021-02-08 12:45:43 +0800

    加入專案檔案。

---
+++ crossroads/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace crossroads
+{
+    partial class crossroads
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RowLight = new System.Windows.Forms.PictureBox();
+            this.ColumnLight = new System.Windows.Forms.PictureBox();
+            this.TrafficLight = new System.Windows.Forms.Timer(this.components);
+            this.VehicleTimer = new System.Windows.Forms.Timer(this.components);
+            this.road = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.RandomTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RowLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RowLight
+            // 
+            this.RowLight.Image = global::crossroads.Properties.Resources.green1;
+            this.RowLight.Location = new System.Drawing.Point(52, 101);
+            this.RowLight.Name = "RowLight";
+            this.RowLight.Size = new System.Drawing.Size(54, 152);
+            this.RowLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RowLight.TabIndex = 3;
+            this.RowLight.TabStop = false;
+            // 
+            // ColumnLight
+            // 
+            this.ColumnLight.Image = global::crossroads.Properties.Resources.red;
+            this.ColumnLight.Location = new System.Drawing.Point(427, 73);
+            this.ColumnLight.Name = "ColumnLight";
+            this.ColumnLight.Size = new System.Drawing.Size(152, 54);
+            this.ColumnLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ColumnLight.TabIndex = 1;
+            this.ColumnLight.TabStop = false;
+            // 
+            // TrafficLight
+            // 
+            this.TrafficLight.Interval = 1000;
+            this.TrafficLight.Tick += new System.EventHandler(this.TrafficLight_Tick);
+            // 
+            // VehicleTimer
+            // 
+            this.VehicleTimer.Interval = 10;
+            this.VehicleTimer.Tick += new System.EventHandler(this.VehicleTimer_Tick);
+            // 
+            // road
+            // 
+            this.road.Image = global::crossroads.Properties.Resources.road;
+            this.road.Location = new System.Drawing.Point(0, 0);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(703, 703);
+            this.road.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.road.TabIndex = 5;
+            this.road.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(734, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(743, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RandomTimer
+            // 
+            this.RandomTimer.Interval = 1000;
+            this.RandomTimer.Tick += new System.EventHandler(this.RandomTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(749, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // crossroads
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(882, 703);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ColumnLight);
+            this.Controls.Add(this.RowLight);
+            this.Controls.Add(this.road);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "crossroads";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RowLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ColumnLight;
+        private System.Windows.Forms.PictureBox RowLight;
+        private System.Windows.Forms.Timer TrafficLight;
+        private System.Windows.Forms.Timer VehicleTimer;
+        private System.Windows.Forms.PictureBox road;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer RandomTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ crossroads/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crossroads
+{
+    public partial class crossroads : Form
+    {
+        public crossroads()
+        {
+            InitializeComponent();
+        }
+
+        private PictureBox vehicle = new PictureBox();//動態生成車子
+        private PictureBox vehicle1 = new PictureBox();
+
+        private int total = 1;
+        private int car = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrafficLight.Enabled = true;
+            VehicleTimer.Enabled = true;
+        }
+
+        private void TrafficLight_Tick(object sender, EventArgs e)
+        {//紅綠燈
+            if (total > 0 && total <= 10)
+            {//行綠列紅
+                ColumnLight.Image = Properties.Resources.red;
+                RowLight.Image = Properties.Resources.green1;
+            }
+            else if (total > 10 && total <= 12)
+            {//行黃
+                RowLight.Image = Properties.Resources.yellow1;
+            }
+            else if (total > 12 && total <= 14)
+            {//行列皆紅
+                RowLight.Image = Properties.Resources.red1;
+            }
+            else if (total > 14 && total <= 24)
+            {//行紅列綠
+                ColumnLight.Image = Properties.Resources.green;
+            }
+            else if (total > 24 && total <= 26)
+            {//列黃
+                ColumnLight.Image = Properties.Resources.yellow;
+            }
+            else if (total > 26 && total <= 28)
+            {//行列皆紅
+                ColumnLight.Image = Properties.Resources.red;
+            }
+            else
+            {//初始化
+                RowLight.Image = Properties.Resources.green1;
+                total = 1;
+            }
+            label1.Text = total.ToString();
+            total++;
+        }
+
+        private void VehicleTimer_Tick(object sender, EventArgs e)
+        {//車子
+            if (total > 0 && total <= 11)
+            {
+                //左右車子通行
+                driving(vehicle, -2);
+                driving(vehicle1, 2);
+            }
+            else if (total > 11 && total <= 14)
+            {
+                //停止通行
+                if (vehicle.Left != 460 && vehicle.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle,-2);
+                if (vehicle1.Left + 75 != 190 && vehicle1.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle1, 2);
+            }
+            else if (total > 14 && total <= 25)
+            {
+                //上下車通行
+                if (vehicle.Left != 460 && vehicle.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle, -2);
+                if (vehicle1.Left + 75 != 190 && vehicle1.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle1, 2);
+            }
+            else if (total > 25 && total <= 28)
+            {
+                //停止通行
+                if (vehicle.Left != 460 && vehicle.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle, -2);
+                if (vehicle1.Left + 75 != 190 && vehicle1.SizeMode == PictureBoxSizeMode.StretchImage)
+                    driving(vehicle1, 2);
+            }
+            else
+            {
+                //初始化
+            }
+        }
+
+        private void driving(object sender, int i)
+        {
+            //先用sender取得控制項名稱，再將sender的類型object轉成該控制項的類型picture
+            //，然後讓vehicle變數接收
+            PictureBox vehicle = (PictureBox)sender;
+
+            vehicle.Left += i;
+            if (vehicle.Left + 74 <= road.Left || vehicle.Left >= 701)
+                road.Controls.Remove(vehicle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vehicle.Location = new Point(700, 275);//位置
+            vehicle.Image = Properties.Resources.redcar;//圖片
+            vehicle.Size = new Size(75, 52);//尺寸
+            vehicle.SizeMode = PictureBoxSizeMode.StretchImage;//填滿
+            vehicle.Parent = road;//父元件(配合透明)
+            vehicle.BackColor = Color.Transparent;//透明
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vehicle1.Location = new Point(-75, 360);//位置
+            vehicle1.Image = Properties.Resources.redcar;//圖片
+            vehicle1.Size = new Size(75, 52);//尺寸
+            vehicle1.SizeMode = PictureBoxSizeMode.StretchImage;//填滿
+            vehicle1.Parent = road;//父元件(配合透明)
+            vehicle1.BackColor = Color.Transparent;//透明
+        }
+
+        private void RandomTimer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
